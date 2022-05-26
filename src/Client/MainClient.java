ad3128c2db83ba6d5cf7be8af6afde06ec0e381d
Yunitrish006006 <yunthomas006@gmail.com>
2022-05-26 11:07:00 +0800

    new x

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -2,8 +2,12 @@ package Client;
 
 import GUI.Window;
 
+import javax.swing.*;
 import javax.swing.text.BadLocationException;
-import java.io.IOException;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class MainClient {
     public static void main(String[] args) throws IOException {
@@ -16,12 +20,26 @@ public class MainClient {
         window.linkButton.addActionListener(e -> {
             /*-------------- client cancel connection ----------------*/
             if(!window.linked) {
-                System.out.println(window.IPText.getText());
                 window.linkButton.setText("離線");
                 window.linked = true;
             }
         });
-        while (!window.linked) {System.out.println(" ");}
+        int sec=30,mss=0,ms=0;
+        while (!window.linked) {
+            mss++;
+            if(mss>100000) {
+                ms++;
+                mss=0;
+            }
+            if(ms>10000) {
+                ms=0;
+                sec--;
+                window.content.setText("自動關閉倒數:"+sec);
+            }
+            if(sec<0) {
+                System.exit(0);
+            }
+        }
         window.revalidate();
         /*----------------- Create processor class -------------------*/
         processor = new MSGProcessor(window.IPText.getText(),Integer.parseInt(window.PortText.getText()));
@@ -29,7 +47,6 @@ public class MainClient {
         Receiver.Port = Integer.parseInt(window.PortText.getText())+1;
         Receiver receiver = new Receiver(window,window.IPText.getText());
         ImageReceiver imageReceiver = new ImageReceiver(window,window.IPText.getText());
-
         MSGProcessor temp = processor;
         /*---------- button action is connect to server --------------*/
         window.linkButton.addActionListener(e -> {
@@ -57,7 +74,6 @@ public class MainClient {
                 window.type.setText("");
             }
         });
-
         /*---------- Create & Start message/image thread -----------*/
         Thread receiver_thread = new Thread(receiver);
         receiver_thread.start();
@@ -66,5 +82,148 @@ public class MainClient {
         ImageSender imageSender = new ImageSender(window,window.IPText.getText());
         Thread imageSender_thread = new Thread(imageSender);
         imageSender_thread.start();
+    }
+}
+/*-------- MSGProcessor contains send/receive message/image method -------*/
+class MSGProcessor {
+
+    private Socket socket;
+    public String Destination;
+    public int Port;
+    public InetAddress CIP;
+
+    /*-------------------Constructor------------------------*/
+    public MSGProcessor(String destination,int port) throws IOException {
+        this.Destination = destination;
+        this.Port = port;
+        this.socket = new Socket(destination,port);
+        this.CIP = socket.getInetAddress();
+    }
+    /*---------- Send message through socket ---------------*/
+    public void sendMessage(String toSend) throws Exception {
+        this.socket = new Socket(Destination, Port);
+        OutputStream outputStream = this.socket.getOutputStream();
+        outputStream.write((toSend).getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
+    }
+    /*--------- Receive message from socket ----------------*/
+    public StringBuffer readMessage() throws Exception {
+        this.socket = new Socket(Destination, Port);
+        InputStream inputStream = this.socket.getInputStream();
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            while (true) {
+                int input_result = inputStream.read();
+                if (input_result==-1) break;
+                stringBuffer.append((char)((byte) input_result));
+            }
+        }
+        catch (Exception e) {inputStream.close();}
+        return stringBuffer;
+    }
+    /*--------- Send image through socket ------------------*/
+    public void sendImage(String path) throws IOException {
+        this.socket = new Socket(Destination, Port);
+        OutputStream outputStream = socket.getOutputStream();
+        FileInputStream fileInputStream = new FileInputStream(new File(path));
+        byte[] temp = new byte[1024];int length;
+        while((length=fileInputStream.read(temp))!=-1) {outputStream.write(temp,0,length);}
+        fileInputStream.close();
+        outputStream.close();
+    }
+    /*--------- Receive image from socket ------------------*/
+    public void saveImage(String path) throws IOException {
+        this.socket = new Socket(Destination, Port);
+        InputStream inputStream = socket.getInputStream();
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
+        byte[] temp = new byte[1024];int length;
+        while((length = inputStream.read(temp))!=-1) {fileOutputStream.write(temp,0,length);}
+        fileOutputStream.close();
+        inputStream.close();
+    }
+
+}
+class Receiver implements Runnable{
+    private final Window window;
+    private String Destination;
+    public MSGProcessor processor;
+    public static int Port;
+
+    /*---------------------Constructor-----------------------*/
+    public Receiver(Window window,String destination) throws IOException {
+        this.window = window;
+        this.Destination = destination;
+        processor = new MSGProcessor(destination, Port);
+    }
+    /*------------ Override run() method in thread -------------------*/
+    /*---------- Invoke this method through thread.start() -----------*/
+    @Override
+    public void run() {
+        StringBuffer stringBuffer = null;
+        while(true){
+            /******* receive message from server & show in chat *********/
+            try {stringBuffer = processor.readMessage();} catch (Exception ignored) {}
+            try {window.display("Server["+ processor.Destination +"]",stringBuffer.toString());} catch (BadLocationException ignored) {}
+        }
+    }
+}
+class ImageSender implements Runnable{
+    private final Window window;
+    private String Destination;
+    public static MSGProcessor processor;
+
+    /*----------------------Constructor-------------------------------*/
+    public ImageSender(Window window,String destination) throws IOException {
+        this.window = window;
+        this.Destination = destination;
+        processor = new MSGProcessor(destination,Receiver.Port+1);
+    }
+    /*------------- Override run() method in thread ------------------*/
+    /*--------- Invoke this method through thread.start() ------------*/
+    @Override
+    public void run() {
+        /*------ when send button pressed start sending image --------*/
+        window.send.addActionListener(e -> {
+            if(!window.ImagePathText.getText().equals("")) {
+                try {
+                    /*----------- get image to send ------------------*/
+                    String imagePathText = window.ImagePathText.getText();
+                    window.ImagePathText.setText("");
+                    /*------ send image to server & show in chat -----*/
+                    processor.sendMessage(window.imagePathCache);
+                    window.display("Client(" + processor.CIP + ")", new ImageIcon(imagePathText),imagePathText);
+                    processor.sendImage(imagePathText);
+                } catch (Exception exception) {
+                    JOptionPane.showMessageDialog(null, "No such file", "file error", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+    }
+}
+class ImageReceiver implements Runnable{
+    private Window window;
+    private String Destination;
+    public MSGProcessor processor;
+    /*-------------------- Constructor------------------------*/
+    public ImageReceiver(Window window,String destination) throws IOException {
+        this.window = window;
+        this.Destination = destination;
+        processor = new MSGProcessor(destination,Receiver.Port +2);
+
+    }
+    /*------------- Override run() method in thread ------------------------*/
+    /*--------- Invoke this method through thread.start() ------------------*/
+    @Override
+    public void run() {
+        StringBuffer stringBuffer;
+        while(true){
+            try {
+                stringBuffer = processor.readMessage();
+                processor.saveImage(stringBuffer.toString());
+                window.display("Server["+ processor.Destination + "]",new ImageIcon(stringBuffer.toString()), stringBuffer.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -2,8 +2,13 @@ package Server;
 
 import GUI.Window;
 
+import javax.swing.*;
 import javax.swing.text.BadLocationException;
-import java.io.IOException;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class MainServer {
     public static void main(String[] args) {
@@ -24,11 +29,11 @@ public class MainServer {
         Thread thread = new Thread(receiver);
         thread.start();
         ImageSender imageSender = new ImageSender(window);
-        Thread thread1 = new Thread(imageSender);
-        thread1.start();
+        Thread imageSender_thread = new Thread(imageSender);
+        imageSender_thread.start();
         ImageReceiver imageReceiver = new ImageReceiver(window);
-        Thread thread2 = new Thread(imageReceiver);
-        thread2.start();
+        Thread imageReceiver_thread = new Thread(imageReceiver);
+        imageReceiver_thread.start();
 
         while (true) {
             MSGProcessor server = null;
@@ -57,6 +62,181 @@ public class MainServer {
                     }
                 });
 
+            }
+
+        }
+    }
+}
+/*-------- MSGProcessor contains send/receive message/image method -------*/
+class MSGProcessor {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    public InetAddress CIP;
+    public InetAddress SIP;
+
+    /*-------------------Constructor------------------------*/
+    public MSGProcessor(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        this.socket = this.serverSocket.accept();
+        CIP = socket.getInetAddress();
+        SIP = socket.getLocalAddress();
+    }
+    /*---------- Send message through socket ---------------*/
+    public void sendMessage(String msg) throws Exception {
+        this.socket = this.serverSocket.accept();
+        OutputStream outputStream = this.socket.getOutputStream();
+        outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
+        socket.close();
+    }
+    /*--------- Receive message from socket ----------------*/
+    public StringBuffer readMessage() throws Exception {
+        this.socket = this.serverSocket.accept();
+        InputStream inputStream = this.socket.getInputStream();
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            while (true) {
+                int temp = inputStream.read();
+                if (temp==-1) break;
+                stringBuffer.append((char) ((byte) temp));
+            }
+        }
+        catch (Exception e) {inputStream.close();}
+        socket.close();
+        return stringBuffer;
+    }
+    /*--------- Send image through socket ------------------*/
+    public void sendImage(String pathOfImage) throws IOException {
+        this.socket = this.serverSocket.accept();
+        OutputStream outputStream = socket.getOutputStream();
+        FileInputStream fileInputStream = new FileInputStream(pathOfImage);
+        byte[] temp = new byte[1024];int length;
+        while((length=fileInputStream.read(temp))!=-1){outputStream.write(temp,0,length);}
+        fileInputStream.close();
+        outputStream.close();
+        socket.close();
+    }
+    /*--------- Receive image from socket ------------------*/
+    public void saveImage(String pathOfImage) throws IOException {
+        this.socket = this.serverSocket.accept();
+        InputStream inputStream = socket.getInputStream();
+        FileOutputStream fileOutputStream = new FileOutputStream(pathOfImage);
+        byte[] temp = new byte[1024];int length;
+        while((length = inputStream.read(temp))!=-1){fileOutputStream.write(temp,0,length);}
+        fileOutputStream.flush();
+        inputStream.close();
+        socket.close();
+    }
+}
+class ImageReceiver implements Runnable{
+    Window window;
+    /*-------------------- Constructor------------------------*/
+    public ImageReceiver(Window window){
+        this.window = window;
+    }
+    /*------------- Override run() method in thread ------------------------*/
+    /*--------- Invoke this method through thread.start() ------------------*/
+    @Override
+    public void run() {
+        while(true) {
+            MSGProcessor processor = null;
+            try {processor = new MSGProcessor(Receiver.port+2);} catch (IOException ignored) {}
+            StringBuffer stringBuffer = new StringBuffer();
+
+            while (Receiver.connected && !stringBuffer.toString().equals("disconnected!!!")) {
+                try {
+                    stringBuffer = processor.readMessage();
+                    processor.saveImage(stringBuffer.toString());
+                    window.display("Server[" + processor.SIP + "]", new ImageIcon(stringBuffer.toString()), stringBuffer.toString());
+                } catch (Exception exception) {
+                    try {
+                        window.display("[Debug]",exception.toString());
+                    } catch (BadLocationException ignored) {}
+                }
+            }
+        }
+    }
+}
+class ImageSender implements Runnable{
+    Window window;
+    /*----------------------Constructor-------------------------------*/
+    public ImageSender(Window window){
+        this.window = window;
+    }
+    /*------------- Override run() method in thread ------------------*/
+    /*--------- Invoke this method through thread.start() ------------*/
+    @Override
+    public void run() {
+        while (true) {
+            MSGProcessor processor = null;
+            try {
+                processor = new MSGProcessor(Receiver.port+3);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            MSGProcessor msgProcessor = processor;
+            while (Receiver.connected) {
+                window.send.addActionListener(e -> {
+                    if(!window.ImagePathText.getText().equals("")) {
+                        try {
+                            String imagePathText = window.ImagePathText.getText();
+                            window.ImagePathText.setText("");
+                            msgProcessor.sendMessage(window.imagePathCache);
+                            System.out.println(window.imagePathCache);
+                            window.display("Server(" + msgProcessor.SIP + ")", new ImageIcon(imagePathText), imagePathText);
+                            msgProcessor.sendImage(imagePathText);
+                        }
+                        catch (Exception ignored) {}
+                    }
+                });
+            }
+        }
+    }
+}
+class Receiver implements Runnable{
+    private Window window;
+    public static boolean connected = true;
+    public static int port= 5501;
+
+    /*---------------------Constructor-----------------------*/
+    public Receiver(Window window){
+        this.window = window;
+    }
+
+    /*------------ Override run() method in thread -------------------*/
+    /*---------- Invoke this method through thread.start() -----------*/
+    @Override
+    public void run() {
+        while(true) {
+            System.out.println("ReceiveMain");
+            MSGProcessor server = null;
+            try {
+                server = new MSGProcessor(port);
+                window.IPText.setText(server.CIP.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            connected = true;
+            while (connected) {
+                StringBuffer msg = null;
+                try {
+                    msg = server.readMessage();
+                    if (msg.toString().equals("disconnected!!!")) {
+                        port+=5;
+                        window.PortText.setText(String.valueOf(port));
+                        connected = false;
+                        window.revalidate();
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    window.display("Client[" + server.CIP + "]", msg.toString());
+                } catch (BadLocationException e) {
+                    throw new RuntimeException(e);
+                }
+                window.revalidate();
             }
 
         }
